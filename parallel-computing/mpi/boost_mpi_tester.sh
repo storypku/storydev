756fee9b62bf3e3a97f214eec4b7042c819cbcd6
storypku <storypku@gmail.com>
2020-06-05 19:03:48 +0800

    Boost.MPI: add boost.mpi test example

---
+++ parallel-computing/mpi/boost_mpi_tester.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+IFS='' read -r -d '' HELLO_MPI_TXT << EOF
+#include <boost/mpi.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  boost::mpi::environment env{argc, argv};
+  boost::mpi::communicator world;
+  std::cout << world.rank() << ", " << world.size() << '\n';
+}
+EOF
+
+set -x
+
+SYSROOT_DIR=/opt/apollo/sysroot
+mpi_test_cpp="$(mktemp /tmp/hello.XXXXXX.cpp)"
+echo "${HELLO_MPI_TXT}" > "${mpi_test_cpp}"
+${SYSROOT_DIR}/bin/mpic++ "${mpi_test_cpp}" -o hello_mpi \
+	-L${SYSROOT_DIR}/lib \
+	-lboost_mpi
+
+${SYSROOT_DIR}/bin/mpirun -np 2 ./hello_mpi
+rm -rf hello_mpi
+set +x
