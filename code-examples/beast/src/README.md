e6704e5e0aabd00ee4e123b8de71fb34aa6c738f
Liu Jiaming <jiaming@qcraft.ai>
2021-07-19 01:26:07 +0800

    Beast: added async http client example
    https://www.boost.org/doc/libs/1_76_0/libs/beast/example/http/client/async/http_client_async.cpp

---
+++ code-examples/beast/src/README.md
@@ -0,0 +1,3 @@
+# Http Async Client
+https://www.boost.org/doc/libs/1_76_0/libs/beast/example/http/client/async/http_client_async.cpp
+
--- code-examples/beast/src/websocket_sync_client.cc
+++ code-examples/beast/src/websocket_sync_client.cc
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <string>
 
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
+#include "boost/asio/connect.hpp"
+#include "boost/asio/ip/tcp.hpp"
+#include "boost/beast/core.hpp"
+#include "boost/beast/websocket.hpp"
 
-namespace beast = boost::beast;          // from <boost/beast.hpp>
-namespace http = beast::http;            // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket;  // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;             // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;        // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;          // from "boost/beast.hpp"
+namespace http = beast::http;            // from "boost/beast/http.hpp"
+namespace websocket = beast::websocket;  // from "boost/beast/websocket.hpp"
+namespace net = boost::asio;             // from "boost/asio.hpp"
+using tcp = boost::asio::ip::tcp;        // from "boost/asio/ip/tcp.hpp"
 
 // Sends a WebSocket message and prints the response
 int main(int argc, char** argv) {
